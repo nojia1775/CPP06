7e939d22879f159efae1bc2b1f767aaf4ed9ee25
moi <125820548+nojia1775@users.noreply.github.com>
2024-12-09 11:53:18 +0000

    relou

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -1,16 +1,24 @@
 #include "ScalarConverter.hpp"
 
-ScalarConverter::ScalarConverter(void)
+ScalarConverter::ScalarConverter(void) : _nbrc(NULL), _nbrd(NULL), _nbrf(NULL), _nbri(NULL)
 {
 	return;
 }
 
 ScalarConverter::~ScalarConverter(void)
 {
+	if (_nbrc)
+		delete _nbrc;
+	if (_nbrd)
+		delete _nbrd;
+	if (_nbrf)
+		delete _nbrf;
+	if (_nbri)
+		delete _nbri;
 	return;
 }
 
-ScalarConverter::ScalarConverter(const ScalarConverter& other)
+ScalarConverter::ScalarConverter(const ScalarConverter& other) : _nbrc(other._nbrc), _nbrd(other._nbrd), _nbrf(other._nbrf), _nbri(other._nbri)
 {
 	(void)other;
 }
@@ -21,13 +29,30 @@ ScalarConverter&	ScalarConverter::operator=(const ScalarConverter& other)
 	return *this;
 }
 
-void	ScalarConverter::convert(const std::string str) const
+bool	ScalarConverter::isChar(const std::string str) const
 {
-	int num;
+	if (str.size() != 1)
+		return false;
+	if (isdigit(str[0]))
+		return false;
+	return true;
+}
 
-	try
+bool	ScalarConverter::isInt(const std::string str) const
+{
+	if (str.size() < 1 || str.size() > 11)
+		return false;
+	for (int i = 0 ; i < str.size() ; i++)
+		if (!isdigit(str[i]))
+			return false;
+	return true;
+}
+
+bool	ScalarConverter::isDouble(const std::string str) const
+{
+	if (str == "inf" || str == "nan")
 	{
-		num = static_cast<int>(str);
+		return true;
 	}
 	
 }
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -7,14 +7,24 @@
 class	ScalarConverter
 {
 	private:
+		char			*_nbrc;
+		double			*_nbrd;
+		float			*_nbrf;
+		int			*_nbri;
+
 					ScalarConverter(void);
 					~ScalarConverter(void);
 
 					ScalarConverter(const ScalarConverter& other);
 		ScalarConverter&	operator=(const ScalarConverter& other);
 
+		bool			isInt(const std::string str) const;
+		bool			isChar(const std::string str) const;
+		bool			isFloat(const std::string str) const;
+		bool			isDouble(const std::string str) const;
+
 	public:
-		void			convert(const std::string num) const;
+		static void		convert(const std::string str);
 };
 
 #endif
